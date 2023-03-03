ed19f88f5eac9fbea7d7906f8a97c2cbe90c1822
Juan Edi <juan@noredink.com>
2023-03-03 12:10:59 -0300

    fetch all the required information from the dom

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,15 @@
-console.log("Did this work?")
+function commitShaFromGithubPath(pathname) {
+  const match = pathname.match(/\/commits\/([a-f0-9]{40})$/);
+  return match ? match[1] : null;
+}
+
+const allCommits =
+      Array
+      .from(document.querySelectorAll(".js-diffbar-range-list a"))
+      .map((link) => commitShaFromGithubPath(link.attributes.href.value));
+
+const currentCommit = commitShaFromGithubPath(window.location.pathname);
+
+console.log("Total commit count:", allCommits.length);
+console.log("Current commit id:", currentCommit);
+console.log("All commits", allCommits);
