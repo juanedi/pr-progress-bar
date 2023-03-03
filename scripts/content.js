78d5f29ced5664b22d96897e8aff759332f02458
Juan Edi <juan@noredink.com>
2023-03-03 14:12:07 -0300

    watch for SPA navigation events

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,35 @@
 function commitShaFromGithubPath(pathname) {
-  const match = pathname.match(/\/commits\/([a-f0-9]{40})$/);
+  const pattern = /.*\/.*\/pull\/[0-9]+\/commits\/([a-f0-9]{40})$/;
+  const match = pathname.match(pattern);
   return match ? match[1] : null;
 }
 
-const allCommits = Array.from(
-  document.querySelectorAll(".js-diffbar-range-list a")
-).map((link) => commitShaFromGithubPath(link.attributes.href.value));
+function onPageLoad() {
+  const currentCommit = commitShaFromGithubPath(window.location.pathname);
+  if (currentCommit) {
+    window.requestAnimationFrame(() => {
+      const allCommits = Array.from(
+        document.querySelectorAll(".js-diffbar-range-list a")
+      ).map((link) => commitShaFromGithubPath(link.attributes.href.value));
 
-const currentCommit = commitShaFromGithubPath(window.location.pathname);
-const currentCommitIndex = allCommits.findIndex((c) => c == currentCommit);
+      const currentCommitIndex = allCommits.findIndex(
+        (c) => c == currentCommit
+      );
 
-console.log("Total commit count:", allCommits.length);
-console.log("Current commit id:", currentCommit);
-console.log("Index of current commit", currentCommitIndex);
+      console.log("Total commit count:", allCommits.length);
+      console.log("Current commit id:", currentCommit);
+      console.log("Index of current commit", currentCommitIndex);
+    });
+  }
+}
+
+onPageLoad();
+
+let lastUrl = window.location.href;
+new MutationObserver(() => {
+  const url = window.location.href;
+  if (url !== lastUrl) {
+    lastUrl = url;
+    onPageLoad();
+  }
+}).observe(document, { subtree: true, childList: true });
